018e37e22bfcbd22c07f8bff8cbc6b6c5cf277f5
Thomas_J <106536735+ThomasJoudinaud@users.noreply.github.com>
2023-03-03 12:28:10 +0100

    some adds to the broken project
    modal and broken token

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script-connexion.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script-connexion.js
@@ -1,32 +1,41 @@
-let result = fetch("http://localhost:5678/api/users/login")
+// fonction pour récupérer les information du formulaire de connexion
 
-// fonction pour l'envoie des identifiants
+  async function logIn() {
+    const formLog = document.querySelector("#formulaireConnexion")
+    formLog.addEventListener("submit", function(event) {
+      event.preventDefault()
 
-function connectUser () {
-    const log = document.querySelector("#formulaireConnexion")
-    log.addEventListener("submit", function(event){
-        event.preventDefault()
-        //creation de l'objet envoyé
-        const user = {
-            email: event.target.querySelector("[name=mail]").value,
-            password: event.target.querySelector("[name=mdp]").value
+      const information = {
+        email:event.target.querySelector("#mail").value,
+        password:event.target.querySelector("#mdp").value
+      }
+//on Fetch avec les informations récupérer ci-dessus
+      async function fetchLogin () {
+        const r = await fetch("http://localhost:5678/api/users/login",{
+          method: "POST",
+          headers: {
+            "Accept" : "application/json",
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(information)
+        })
+        try{
+          if (r.ok === true) {
+            return r.json()
+          }
+          alert("Email ou mot de passe invalide")
+        } catch (e) {
+          console.error("Erreur Serveur")
         }
-        //creation de la charge utile
-        const objectUser = JSON.stringify(user)
-
-        // envoie
-
-        fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            headers: {"Content-Type" : "application/json"},
-            body: objectUser
+    
+      }
+      fetchLogin()
+      //je créer un sessionStorage du token pour garder la connexion tout en limitant les possibles intrusions non voulu qu'aurait pu causer un localStorage
+        .then(user => {
+          sessionStorage.setItem("token", JSON.stringify(user.token))
+          location.replace("./index.html")
         })
     })
-
-}
-
-connectUser()
-
-
-
+  }
+logIn()
 
--- Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/script.js
@@ -1,6 +1,7 @@
+const token = sessionStorage.getItem("token")
+console.log(token)
 
 // Je créer mon lien avec l'API ou se trouve les travaux de Sophie
-
 const travaux = await fetch("http://localhost:5678/api/works")
     .then(travaux => travaux.json())
     
@@ -29,7 +30,6 @@ function creerCarte (travaux) {
 
         //Lien entre l'élement enfant et l'élément parent
         gallery.appendChild(carte)
-
         carte.appendChild(imageCarte)
         carte.appendChild(titreCarte)
 
@@ -38,7 +38,6 @@ function creerCarte (travaux) {
 
 // Appel de la fonction pour créer les cartes dynamiques
 creerCarte(travaux)
-
 
 // les filtres des travaux
 
@@ -75,3 +74,118 @@ boutonRestaurant.addEventListener("click", function(){
         document.querySelector(".gallery").innerHTML =""
         creerCarte(filtreRestaurant)
 })
+
+//utilité du bouton logout
+const logout = document.querySelector("#logoutButton")
+logout.addEventListener("click", function(){
+    sessionStorage.clear()
+})
+
+
+//Modification de la page d'acceuil quand l'utilisateur est log
+const tokenStorage = sessionStorage.token
+
+if(tokenStorage !== undefined){
+    const filtreAdmin = document.querySelector("#filtre")
+    filtreAdmin.style.display = "none"
+    const loginButton = document.querySelector("#loginButton")
+    loginButton.style.display = "none"
+    const logoutButton = document.querySelector("#logoutButton")
+    logoutButton.style.display = "flex"
+    const modifierClass = document.querySelector("#modifier")
+    modifierClass.style.display = "flex"
+    const modifierClass2 = document.querySelector("#modifier2")
+    modifierClass2.style.display = "flex"
+    const headerValidation = document.querySelector("#modeEdition")
+    headerValidation.style.display = "flex"
+}
+
+//Affichage de la modale
+let defaultModal = null
+
+const openModal = function(e) {
+    e.preventDefault()
+    const target = document.querySelector("#modalAdmin")
+    target.style.display = null
+    target.removeAttribute("aria-hidden")
+    target.setAttribute("aria-modal", "true")
+    defaultModal = target
+    defaultModal.addEventListener("click", closeModal)
+    defaultModal.querySelector("#closeModalButton").addEventListener("click", closeModal)
+    defaultModal.querySelector("#modal").addEventListener("click", stopPropagation)
+}
+
+const closeModal = function (e) {
+    if (defaultModal === null) return
+    e.preventDefault()
+    defaultModal.style.display = "none"
+    defaultModal.setAttribute("aria-hidden", "true")
+    defaultModal.removeAttribute("aria-modal")
+    defaultModal.removeEventListener("click", closeModal)
+    defaultModal.querySelector("#closeModalButton").removeEventListener("click", closeModal)
+    defaultModal.querySelector("#closeButtonDiv").removeEventListener("click", stopPropagation)
+
+    defaultModal = null
+}
+
+const stopPropagation = (e) => {
+    e.stopPropagation()
+}
+
+document.querySelectorAll(".js-modifier").forEach(a => {
+    a.addEventListener("click", openModal)
+})
+
+window.addEventListener("keydown", function(e){
+    if(e.key === "Escape" || e.key === "Esc") {
+        closeModal(e)
+    }
+})
+
+
+function adminPanel (travaux) {
+    for (let i = 0; i < travaux.length; i++) {
+
+        const travail = travaux[i]
+        
+
+        const galleryAdmin = document.querySelector("#adminWork")
+
+
+        const carteAdmin = document.createElement("figure")
+        carteAdmin.dataset.id = travail.id
+
+        const imageCarte = document.createElement("img")
+        imageCarte.src = travail.imageUrl
+
+        const titreCarte = document.createElement("p")
+        titreCarte.innerText = travail.title
+
+        const trashButton = document.createElement("button")
+        trashButton.setAttribute("id", "trashClick")
+
+
+        galleryAdmin.append(carteAdmin)
+        carteAdmin.append(trashButton)
+        carteAdmin.append(imageCarte)
+        carteAdmin.append(titreCarte)
+    }
+}
+adminPanel(travaux)
+
+const deleteWork = async function (e) {
+    await fetch("http://localhost:5678/api/works/" + e, {
+        method: "DELETE",
+        headers: {
+            "Authorization" :  token
+        }  
+    })
+}
+
+const trashWork = document.querySelectorAll("#trashClick").forEach( e => {
+    e.addEventListener("click", () => {
+        const result = e.parentElement.dataset.id
+        deleteWork(result)
+    })
+})
+
